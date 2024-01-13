8ad553dc01610b5c262bd11e6b2b31f3314a0861
tal <talevi100@gmail.com>
2024-01-13 13:04:14 +0100

    added client to class

--- src/DiscogsApiWrapper.php
+++ src/DiscogsApiWrapper.php
@@ -2,7 +2,24 @@
 
 namespace Thespacecadet\DiscogsApiWrapper;
 
+
+
+use GuzzleHttp\Client;
+
 class DiscogsApiWrapper
 {
 
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+    }
+
+//    public function check(){
+//        $client = new \GuzzleHttp\Client();
+//    }
 }
